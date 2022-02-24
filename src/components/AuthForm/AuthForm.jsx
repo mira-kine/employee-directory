55afd154055377af9e442cad6ffdf648feb497f7
mira-kine <mirak0419@gmail.com>
2022-02-23 18:00:50 -0800

    working on the form

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -2,16 +2,32 @@ import React from 'react';
 import { useForm } from '../../hooks/useForm';
 // form will be used for both register and login, depending on
 export default function AuthForm({ onSubmit }) {
-  const { formState, handleForm, clearForm } = useForm({
-    email: '',
-    password: '',
-  });
-
+  const { formState, handleForm, clearForm, setFormError, formError } = useForm(
+    {
+      email: '',
+      password: '',
+    }
+  );
+  const [loading, setLoading] = useState(false);
   // onSubmit is the handleAuth from Auth.js, API call will depend on
   // which type of form it is (sign in or sign up)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { email, password } = formState;
+    try {
+      setFormError('');
+      setLoading(true);
+      await onSubmit(email, password);
+    } catch (e) {
+      setFormError(e.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Email</label>
         <input
           type="email"
@@ -26,6 +42,7 @@ export default function AuthForm({ onSubmit }) {
           value={formState.password}
           onChange={handleForm}
         />
+        <button type="submit">Sign in or Sign up (change the label)</button>
       </form>
     </>
   );
