34997a6a3adb22fe3446b5c32283f8bf4a2c6544
mira-kine <mirak0419@gmail.com>
2022-02-23 16:35:59 -0800

    created UserContext Provider, Private Route next

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,36 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Home from './views/Home/Home';
+import Auth from './views/Auth/Auth';
+import Profile from './views/Profile/Profile';
+import CreateProfile from './views/CreateProfile/CreateProfile';
+import Edit from './views/Edit/Edit';
+import Register from './views/Register/Register';
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/login">
+          <Auth />
+        </Route>
+        <Route exact path="/register">
+          <Register />
+        </Route>
+        <Route exact path="/profile">
+          <Profile />
+        </Route>
+        <Route exact path="/create-profile">
+          <CreateProfile />
+        </Route>
+        <Route exact path="/edit">
+          <Edit />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
